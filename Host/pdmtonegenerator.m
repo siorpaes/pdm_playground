ddaa46d652a1e5be5add276669c6038d9b7a69aa
David Siorpaes <david.siorpaes@st.com>
2018-05-03 16:09:40 +0200

    Added attenuation factor in PDM generator Octave script

--- Host/pdmtonegenerator.m
+++ Host/pdmtonegenerator.m
@@ -2,22 +2,30 @@ clear all;
 Fs = 1000000;
 Ts = 1/Fs;
 Ftone = 500;
+Attenuation = 16;
 
 % This must be integer!
 Nperiod = Fs/Ftone;
 
+%Generate tone
 n=0:(Nperiod-1);
-tone = sin(2*pi*Ftone*n*Ts)/2 + 0.5;
+tone = sin(2*pi*Ftone*n*Ts);
+tone = tone/Attenuation;
 qe = 0;
 
+%Generate PDM in [-1, 1]
 for i=1:length(tone)
  if ( tone(i) > qe )
    pdm(i) = 1;
  else
-   pdm(i) = 0;
+   pdm(i) = -1;
  endif
  qe = pdm(i) - tone(i) + qe;
 endfor
+
+%Frame in [0, 1]
+pdm = pdm + 1;
+pdm = pdm / 2;
 
 plot(n, pdm, n, tone);
 
