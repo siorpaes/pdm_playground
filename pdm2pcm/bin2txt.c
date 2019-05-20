9d331fa713849cafd93608dfca43222bfd3fc067
David Siorpaes <david.siorpaes@st.com>
2019-05-20 17:18:11 +0200

    Added tool that convers PCM S16_LE samples in text format. Useful for loading data with Matlab/Octave

---
+++ pdm2pcm/bin2txt.c
@@ -0,0 +1,27 @@
+/** @file bin2txt.c
+ * Converts S16_LE input to text
+ * Usage:
+ * bin2txt < audio.bin > audio.txt
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+int main(int argc, char** argv)
+{
+	int ret;
+	int16_t input;
+   
+	ret = 2;
+
+	while(ret == 2){
+		ret = read(0, &input, 2);
+		printf("%i\n", input);
+	}
+	
+	return 0;
+}
+
+
